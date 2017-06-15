b94ed7dc49e890a6a0b528133bc4510f564febeb
PauliusJ-PC\PauliusJ <paulius.juknis@gmail.com>
2017-06-15 21:36:10 +0300

    6as komit

--- js/jqcontrols.js
+++ js/jqcontrols.js
@@ -1,25 +1,36 @@
 // isskleidzia/sutraukia playlista kai paspaudziam ant mygtuko
+
 $(document).ready(function () {
+    function toggleGif() {
+        if (arPasleptasPlay) {
+            $(".gif").fadeOut(3000)
+        } else {
+            $(".gif").fadeIn(1000);
+        }
+        arPasleptasPlay = !arPasleptasPlay;
+    }
     $("#playListButton").click(function () {
         $("#playList").slideToggle("slow");
     });
+    var arPasleptasPlay = false;
     $("#playButton").click(function () {
-        $(".gif").show();
-    })
-
-
-   /*$('#myTrack').on('ended', function () {
-        console.log("groju sekanti");
-        current++;
-        if (current == len) { // jei paskutine daina baigiasi persoka i pirma is saraso
-            current = 0;
-            link = playlist.find('a')[0];
-        } else {
-            link = playlist.find('a')[current];
-        }
-        groti($(link), audio[0]); // paleidzia grojimo funkcija
+        toggleGif();
     });
-*/
+    var arPasleptasMute = false;
+    $("#muteButton").click(function () {
+        if (arPasleptasMute) {
+            $(".gif").show();
+        } else {
+            $(".gif").hide();
+        }
+        arPasleptasMute = !arPasleptasMute;
+    });
+
+
+    $("#playList").find('li').click(function () {
+        $(".gif").fadeIn(1000);
+    });
+
 });
 
 // playlisto sukurimas, kuria spausime daina, ta paleis
@@ -65,11 +76,12 @@ function groti(link, player) {
     par = link.parent();
     par.addClass('active').siblings().removeClass('active');
     audio[0].load();
-    playOrPause(); update(); clickedBar();
+    playOrPause();
+    update();
+    clickedBar();
     audio[0].play();
 
 }
-
 
 // neatvaizduoja pilno laiko naujai uzkrautoms dainoms iskyrus pirma daina
 
