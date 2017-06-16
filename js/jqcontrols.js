7960600225eb07cccd2ac3612dad6d77359dc9c3
PauliusJ-PC\PauliusJ <paulius.juknis@gmail.com>
2017-06-16 19:22:36 +0300

    js taisymai2

--- js/jqcontrols.js
+++ js/jqcontrols.js
@@ -1,47 +1,49 @@
-var audio;
-var playlist;
-var tracks;
-var current;
-var len;
-var link;
-var par;
-
 $(document).ready(function () {
-paleistiPlaylist();
-function paleistiPlaylist() {
-    current = 0;
-    audio = $('audio');
-    playlist = $('#playList');
-    tracks = playlist.find('li a');
-    len = tracks.length;
-    playlist.find('a').click(function (e) { // kai paspaudziam ant dainos nuorodos playliste
-        e.preventDefault();
-        link = $(this);
-        current = link.parent().index(); // nustato kuris dabartinis indeksas masyve
-        groti(link, audio[0]); // paleidzia groti funkcija
-    });
-    audio[0].addEventListener('ended', function (e) {
-        //console.log("groju sekanti");
-        current++;
-        if (current == len) { // jei paskutine daina baigiasi persoka i pirma is saraso
-            current = 0;
-            link = playlist.find('a')[0];
-        } else {
-            link = playlist.find('a')[current];
-        }
-        groti($(link), audio[0]); // paleidzia groti funkcija
-    });
+    var audio;
+    var playlist;
+    var tracks;
+    var current = 0;
+    var len;
+    var link;
+    var par;
+
+    function paleistiPlaylist() {
+        audio = $('audio');
+        playlist = $('#playList');
+        tracks = playlist.find('li a');
+        len = tracks.length;
+        playlist.find('a').click(function (e) { // kai paspaudziam ant dainos nuorodos playliste
+            e.preventDefault();
+            link = $(this);
+            current = link.parent().index(); // nustato kuris dabartinis indeksas masyve
+            groti(link, audio[0]); // paleidzia groti funkcija
+        });
+        audio[0].addEventListener('ended', function (e) {
+            //console.log("groju sekanti");
+            current++;
+            if (current == len) { // jei paskutine daina baigiasi persoka i pirma is saraso
+                current = 0;
+                link = playlist.find('a')[0];
+            } else {
+                link = playlist.find('a')[current];
+            }
+            groti($(link), audio[0]); // paleidzia groti funkcija
+        });
+    }
+
+    paleistiPlaylist();
 
     function groti(link, player) {
+
+        link = $(link);
         player.src = link.attr('href');
         par = link.parent();
         par.addClass('active').siblings().removeClass('active');
         audio[0].load();
-        playOrPause();
+        //playOrPause();
         clickedBar();
         audio[0].play();
     }
-}
 
     function toggleGif() {
         if (arPasleptasPlay) {
@@ -72,29 +74,32 @@ function paleistiPlaylist() {
         $(".gif").fadeIn(1000);
     });
 
-
     // NEVEIKIA ABI FUNKCIJOS BLOGA LOGIKA
     $("#previousTrack").click(function () {
-        if (current == 0) {
-            link = playlist.find('a')[tracks.length-1];
+        console.log(current);
+        if (current === 0) {
+            link = tracks[tracks.length - 1];
             groti(link, audio[0]);
+            current = tracks.length - 1;
         }
         else {
-            link = playlist.find('a')[current-1];
+            link = playlist.find('a')[current - 1];
             groti(link, audio[0]);
+            --current;
         }
     });
 
     $("#nextTrack").click(function () {
         link = playlist.find('a')[current];
-        if (current == link.length) {
-            link = 0;
+        if (current === tracks.length) {
+            link = tracks[0];
             groti(link, audio[0]);
+            current = 0;
         } else {
-            groti((link + 1), audio[0]);
+            groti(link, audio[0]);
+            ++current;
         }
     });
-
 });
 
 
